48af68e14c34652a647ec1d29ea50acbe9f72b07
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-03-20 09:42:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(s){
+	s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+  // iterate over string and compare characters from both ends
+  for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+    if (s[i] !== s[j]) {
+      return false;
+    }
+  }
+  
+  return true;
 }
 module.exports = palindrome
